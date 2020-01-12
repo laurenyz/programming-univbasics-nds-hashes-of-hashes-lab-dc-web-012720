32a0c503b9111be62838942702f437ef21b70759
Lauren Yu <laurenkyu@gmail.com>
2020-01-12 21:19:23 +0000

    Automatically backed up by Learn

--- lib/hoh.rb
+++ lib/hoh.rb
@@ -45,6 +45,7 @@ def naming_system
   naming_system = PORTION_1
   naming_system[:sub_category][:sub_category][:sub_category] = PORTION_2
   naming_system[:sub_category][:sub_category][:sub_category][:sub_category] = PORTION_3
+  naming_system[:sub_category][:sub_category][:sub_category][:sub_category][:sub_category][sub_category] = PORTION_4
 p naming_system
 end
 
